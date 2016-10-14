c023dfec3e903049074cac37ce0cb294d1a83163
Daniel Gatti <Dan.Gatti@jax.org>
2016-10-14 11:13:34 -0400

    Changed over to use machine image

--- scripts/run_one_DO_machine.R
+++ scripts/run_one_DO_machine.R
@@ -5,6 +5,9 @@ Sys.setenv(DO_PAT = "*** REPLACE THIS BY YOUR DIGITAL OCEAN API KEY ***")
 d <- docklet_create(size = getOption("do_size", "8gb"),
                     region = getOption("do_region", "nyc2"))
 
+# Needed to add this to update the IP address. OTherwise, I got a "network not up yet" error.
+d = droplet(d$id)
+
 # pull images
 d %>% docklet_pull("rocker/hadleyverse")
 d %>% docklet_pull("churchill/doqtl")
@@ -14,11 +17,29 @@ d %>% docklet_pull("churchill/webapp")
 d %>% docklet_images()
 
 # download files to /data folder, takes ~30mins
-lines <- "wget https://raw.githubusercontent.com/churchill-lab/sysgen2015/master/scripts/download_data_from_ftp.sh
+lines <- "wget https://raw.githubusercontent.com/churchill-lab/sysgen2016/master/scripts/download_data_from_ftp.sh
           /bin/bash download_data_from_ftp.sh
           rm download_data_from_ftp.sh"
 cmd <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines))
 analogsea:::do_system(d, cmd, verbose = TRUE)
+
+# Make a snapshot of this machine.
+d %>%
+  droplet_power_off() %>%
+  droplet_wait() %>%
+  droplet_snapshot(name = "shortcourse2016")
+
+# Destroy the source droplet to see if we can re-make it using the image.
+droplet_delete(d)
+rm(d)
+
+# Run the one machine.
+img = images(private = TRUE)[["shortcourse2016"]]
+d = droplet_create(name = "droplet1", size = "8gb", image = img[["id"]],
+                   region = "nyc2")
+
+# Needed to add this to update the IP address. OTherwise, I got a "network not up yet" error.
+d = droplet(d$id)
 
 # start the containers
 d %>% docklet_run("-d", " -v /data:/data", " -p 8787:8787", " -e USER=rstudio", " -e PASSWORD=sysgen ", "churchill/doqtl")
