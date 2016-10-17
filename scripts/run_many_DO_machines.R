f42831670e6a85c4d8e93e9721c53c366220b104
Daniel Gatti <Dan.Gatti@jax.org>
2016-10-17 12:53:08 -0400

    Changed DOQTL image from doqtl2016 to sysgen2016

--- scripts/run_many_DO_machines.R
+++ scripts/run_many_DO_machines.R
@@ -34,7 +34,7 @@ for(i in 1:N) {
   d = droplet(droplet_list[[i]]$id)
   
   # start the containers.
-  d %>% docklet_run("-d", " -v /data:/data", " -p 8787:8787", " -e USER=rstudio", " -e PASSWORD=sysgen ", "churchill/doqtl2016")
+  d %>% docklet_run("-d", " -v /data:/data", " -p 8787:8787", " -e USER=rstudio", " -e PASSWORD=sysgen ", "churchill/sysgen2016")
   d %>% docklet_run("-dt", " -v /data:/data", " -p 43210:43210 -p 43211:43211 ", "churchill/asesuite") %>% docklet_ps()
   
   # add symbolic links
