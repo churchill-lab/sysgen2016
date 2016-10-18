ce5ce5222b42d1192bb52475e4ec678fc191deb8
Petr Simecek <lamparna@gmail.com>
2016-10-18 09:26:35 -0400

    subset indices corrected

--- scripts/run_many_DO_machines.R
+++ scripts/run_many_DO_machines.R
@@ -3,7 +3,7 @@ library(parallel)
 library(doParallel)
 Sys.setenv(DO_PAT = "*** REPLACE THIS BY YOUR DIGITAL OCEAN API KEY ***")
 
-participants <- read.delim("SysGen_Patricipant_List_10_11.txt", as.is=TRUE)
+participants <- read.csv("participant_list.txt", sep="\t", as.is=TRUE)
 N = nrow(participants)
 
 # Make machine names based on e-mail addresses.
@@ -22,7 +22,7 @@ droplet_list = droplets_create(names = machine.names[1:10], size = "8gb", image 
                                region = "nyc2")
 droplet_list[11:20] = droplets_create(names =  machine.names[11:20], size = "8gb", image = img[[1]],
                                region = "nyc2")
-droplet_list[21:N] = droplets_create(names =  machine.names[21:30], size = "8gb", image = img,[[1]]
+droplet_list[21:30] = droplets_create(names =  machine.names[21:30], size = "8gb", image = img,[[1]]
                                region = "nyc2")
 droplet_list[31:N] = droplets_create(names =  machine.names[31:N], size = "8gb", image = img[[1]],
                                region = "nyc2")
